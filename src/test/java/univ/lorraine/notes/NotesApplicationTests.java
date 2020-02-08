b49a9839d44f6e3e26a8ce8843819b158e495a27
Dany Sanchez <daany_57@hotmail.fr>
2020-02-08 11:27:59 +0100

    :tada: First commit

---
+++ src/test/java/univ/lorraine/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.lorraine.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
